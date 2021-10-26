9b9eb104df11d5e5a22152c02b369dd48879980b
Luke Shell <llshell27@gmail.com>
2021-10-26 16:17:43 +0100

    Category usage improvement

--- src/Models/Post.php
+++ src/Models/Post.php
@@ -16,5 +16,13 @@ class Post extends Model
     const STATUS_DRAFT = 0;
     const STATUS_PUBLISHED = 1;
 
+    public function categories(){
+        $data = [];
+        foreach ($this->categories as $category){
+            $data[] = PostCategory::where('id', $category)->first();
+        }
+        return $data;
+    }
+
 
 }
